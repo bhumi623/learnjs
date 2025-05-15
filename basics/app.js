67131a65d87a6e90801459cb03af5f234c9a22d6
Bhumi <bhumiagarwal130@gmail.com>
2025-05-15 23:43:38 +0530

    day1

---
+++ basics/app.js
@@ -0,0 +1,33 @@
+//This gives an alert {to comment ctrl + /}
+// alert("Hiiii");
+//This displays in the console 
+console.log("Is it visible?");
+//This clears the console
+console.clear();
+// Here is example of operations, classses, and objects
+console.log("Is it visible?");
+console.log(2+2*2-6/(1+1))
+console.log(2**3);
+console.log({name: "Bhumi"})
+console.warn("This is a warning");
+console.table({name: "Bhumi", age: 18});
+
+
+//variables
+//(reserved keyword) (variable name) (type of value)
+let name;
+console.log(name); //undefined
+name = "Bhumi";
+console.log(name); //Bhumi
+let age = 18;
+console.log(age); //18
+name = "Bhumi Agarwal";
+console.log(name); //Bhumi Agarwal
+const pi = 3.14; //constant
+console.log(pi); //3.14
+// const pi = 3.14159; //error: Identifier 'pi' has already been declared
+console.log(typeof name); //string
+console.log(typeof pi); 
+age ++;//number
+console.log(age);
+
---
+++ basics/task.js
@@ -0,0 +1,25 @@
+//1
+
+let name = "Bhumi";
+let whatDoYouWannaBecomeInYourLife = "Software Engineer";
+let gender = "Female";
+let LinkedinProfile = "www.linkedin.com/in/bhumi-agarwal-7a2ba6307";
+
+console.log(name);
+console.log(whatDoYouWannaBecomeInYourLife);
+console.log(gender);
+console.log(LinkedinProfile);
+
+//2
+
+let firstFavNum = 7;
+let secondFavNum = 13;
+console.log(firstFavNum + secondFavNum);
+console.log(firstFavNum - secondFavNum);
+console.log(firstFavNum * secondFavNum);
+console.log(firstFavNum / secondFavNum);
+console.log(firstFavNum % secondFavNum);
+console.log(firstFavNum ** secondFavNum);
+
+
+
